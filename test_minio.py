9dcf09a0cc84f20d0483043b1ea9272a0d6dd3f8
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2024-08-18 14:55:17 +0200

    add tests

--- test_minio.py
+++ test_minio.py
@@ -2,27 +2,24 @@ import configparser
 
 from s3_service import s3_bucket_service_factory
 
+
+OBJECTS_TO_UPLOAD = [1, 2, 3]
 config = configparser.ConfigParser()
 config.read("default.ini")
+S3 = s3_bucket_service_factory(config)
 
 
 def test_object_is_created():
-    s3 = s3_bucket_service_factory(config)
-    objects_to_upload = [1, 2, 3]
+    for obj in OBJECTS_TO_UPLOAD:
+        S3.upload_file_object("test", f"{obj}.txt", "")
 
-    for obj in objects_to_upload:
-        s3.upload_file_object("test", f"{obj}.txt", "")
-
-    objects_in_bucket = s3.list_objects("test")
-    assert len(objects_in_bucket) == len(objects_to_upload)
+    objects_in_bucket = S3.list_objects("test")
+    assert len(objects_in_bucket) == len(OBJECTS_TO_UPLOAD)
 
 
 def test_object_is_deleted():
-    s3 = s3_bucket_service_factory(config)
-    objects_to_upload = [1, 2, 3]
+    for obj in OBJECTS_TO_UPLOAD:
+        S3.delete_file_object("test", f"{obj}.txt")
 
-    for obj in objects_to_upload:
-        s3.delete_file_object("test", f"{obj}.txt")
-
-    objects_in_bucket = s3.list_objects("test")
+    objects_in_bucket = S3.list_objects("test")
     assert len(objects_in_bucket) == 0
